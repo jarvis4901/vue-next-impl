98aad06d0de1333cbbbcf1ecb93f6f77c8b43422
guojiahui <jarvis4901@sina.com>
2020-11-26 16:58:27 +0800

    fix：setter中trigger获取的值滞后

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
-import { reactivity, effect } from "../lib/reactivity";
-let person = reactivity({
-  name: "郭家辉",
-  age: 26,
-  sex: 1,
-  hobby: ["games", "sports", "singing"],
-  company: [
-    {
-      id: 1,
-      companyName: "微软",
-      rate: 20.8,
-    },
-    {
-      id: 2,
-      companyName: "阿里巴巴",
-      rate: 10.2,
-    },
-    {
-      id: 3,
-      companyName: "华为",
-      rate: 9.2,
-    },
-  ],
-});
+import { reactivity, effect, computed } from "../lib/reactivity";
 
-// let arr = reactivity([{ id: 1 }, { id: 2 }, { id: 3 }]);
+let product = reactivity({ count: 1, price: 5.0 });
+// let totalPirce = computed(() => product.count * product.price);
 
-person.name = "郭家辉2号";
 effect(() => {
-  // console.log(effect);
-  console.log("effected");
+  console.log(product.count * product.price);
 });
+
+product.count = 3;
+console.log(product);
+// console.log(totalPirce.value);
+// let arr = reactivity([{ id: 1 }, { id: 2 }, { id: 3 }]);
