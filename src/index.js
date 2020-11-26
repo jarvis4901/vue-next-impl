2c54e08ed2ee18ef09c3f257cf2b9c68ad88815a
guojiahui <jarvis4901@sina.com>
2020-11-26 17:03:20 +0800

    docs: 创建effect时的lazy的注释

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { reactivity, effect, computed } from "../lib/reactivity";
 
 let product = reactivity({ count: 1, price: 5.0 });
-// let totalPirce = computed(() => product.count * product.price);
+let totalPirce = computed(() => product.count * product.price);
 
-effect(() => {
-  console.log(product.count * product.price);
-});
+// effect(() => {
+//   console.log(product.count * product.price);
+// });
 
 product.count = 3;
-console.log(product);
-// console.log(totalPirce.value);
+// console.log(product);
+console.log(totalPirce.value);
 // let arr = reactivity([{ id: 1 }, { id: 2 }, { id: 3 }]);
